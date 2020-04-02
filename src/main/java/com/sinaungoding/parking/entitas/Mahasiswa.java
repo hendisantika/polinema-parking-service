bb537c5fd5583e799be62e829e2418da467fb2e4
od3ng <noprianto@polinema.ac.id>
2020-04-03 00:59:49 +0700

    tambahan copyright

--- src/main/java/com/sinaungoding/parking/entitas/Mahasiswa.java
+++ src/main/java/com/sinaungoding/parking/entitas/Mahasiswa.java
@@ -1,3 +1,15 @@
+/*
+ * polinema-parking-service
+ *
+ * Copyright (c) 2020
+ * All rights reserved
+ * Written by od3ng created on 4/3/20, 12:58 AM
+ * Blog    : sinaungoding.com
+ * Email   : noprianto@polinema.ac.id
+ * Github  : 0d3ng
+ * Hp      : 085878554150
+ */
+
 package com.sinaungoding.parking.entitas;
 
 import lombok.Data;
--- src/main/java/com/sinaungoding/parking/entitas/Rekap.java
+++ src/main/java/com/sinaungoding/parking/entitas/Rekap.java
@@ -1,17 +1,30 @@
+/*
+ * polinema-parking-service
+ *
+ * Copyright (c) 2020
+ * All rights reserved
+ * Written by od3ng created on 4/3/20, 12:58 AM
+ * Blog    : sinaungoding.com
+ * Email   : noprianto@polinema.ac.id
+ * Github  : 0d3ng
+ * Hp      : 085878554150
+ */
+
 package com.sinaungoding.parking.entitas;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @Entity
 public class Rekap {
     @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(length = 40)
     private String id;
     private String serial;
     private String idKantong;
